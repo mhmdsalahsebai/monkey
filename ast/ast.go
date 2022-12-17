89e8d10252f2018c4b2a918a749b2d921f7d02b2
Mohamed Salah <mohamedsalahsebai@gmail.com>
2022-12-17 22:57:47 +0200

    Adding the Parser - parsing let statement

---
+++ ast/ast.go
@@ -0,0 +1,56 @@
+package ast
+
+import (
+	"monkey/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+/*
+This Program node is going to be the root node of every AST our parser produces. Every valid Monkey program is a series of statements.
+These statements are contained in the Program.Statements, which is just a slice of AST nodes that implement the Statement interface.
+*/
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
